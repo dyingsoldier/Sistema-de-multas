c0fb573defba7cd5605f5fe4a8a76b75e2fee6f2
Manoel Pedro <manoelpedromn@hotmail.com>
2025-05-05 11:08:11 -0300

    armazenando

---
+++ script.js
@@ -0,0 +1,27 @@
+let button = document.getElementById("button")
+
+button.addEventListener("click", function () {
+  let velCarro = document.getElementById("velCarro")
+  let velPista = document.getElementById("velPista")
+  let tittle = document.getElementsByTagName("h1")[0]
+
+  let carroVel = Number(velCarro.value)
+  let pistaVel = Number(velPista.value)
+
+  let res = document.getElementById("res")
+
+  console.log(carroVel)
+  console.log(pistaVel)
+
+  if (carroVel >= pistaVel) {
+    tittle.innerText = `Foi de multas amigo`
+    res.innerHTML = `<p> Você está acima da velocidade da pista, então foi multado </p>
+    <p> a velocidade era de <strong> ${carroVel} km/h </strong> numa pista de <strong> ${pistaVel} km/h </strong> </p>`
+  } else {
+    tittle.innerText = `Quase em amigo`
+    res.innerHTML = `<p> Você estava abaixo da velocidade da pista, então não fez mais que sua obrigação! </p> <p> Sua velocidade era de <strong> ${carroVel} km/h </strong> numa pista de <strong> ${pistaVel} km/h </strong> </p>`
+  }
+
+  velCarro.value = ""
+  velPista.value = ""
+})
